42e5d6963a939f887bd1c71d368a97fe2412daba
Ruslan Shakirov <shakirovrrr@outlook.com>
2020-02-06 16:01:53 +0300

    Add history

--- SOLID-Calculator/src/UserRepl.java
+++ SOLID-Calculator/src/UserRepl.java
@@ -3,6 +3,8 @@ import Parser.ExpressionParser;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class UserRepl {
@@ -10,7 +12,7 @@ public class UserRepl {
 	private final ExpressionParser parser;
 	private final InputStream inputStream;
 	private final PrintStream printStream;
-
+	private final Queue<String> history;
 
 	public UserRepl(CalculatorCore calcCore, ExpressionParser parser) {
 		this(calcCore, parser, System.in, System.out);
@@ -21,6 +23,7 @@ public class UserRepl {
 		this.parser = parser;
 		this.inputStream = inputStream;
 		this.printStream = printStream;
+		this.history = new LinkedList<>();
 	}
 
 	public void run() {
@@ -29,12 +32,32 @@ public class UserRepl {
 		boolean parsingSucceeded;
 		char operator;
 		double a, b;
+		StringBuilder stringBuilder = new StringBuilder();
+		double result;
+
+		printStream.println("hist - show history.\nexit - exit.\n+, -, *, / are allowed.\n");
+
 		while (true) {
-			input = scanner.nextLine();
+			input = scanner.nextLine().strip();
+			if (input.length() == 0) {
+				continue;
+			}
 
 			if (input.toLowerCase().equals("exit")) {
 				printStream.println("Exit.");
 				break;
+			}
+
+			if (input.toLowerCase().equals("hist")) {
+				if (history.isEmpty()) {
+					printStream.println("No items in history.\n");
+					continue;
+				}
+				for (String s : history) {
+					printStream.println(s);
+				}
+				printStream.println();
+				continue;
 			}
 
 			parser.setInput(input);
@@ -50,21 +73,32 @@ public class UserRepl {
 
 			switch (operator) {
 				case '+':
-					printStream.println(calcCore.add(a, b));
+					result = calcCore.add(a, b);
 					break;
 				case '-':
-					printStream.println(calcCore.subtract(a, b));
+					result = calcCore.subtract(a, b);
 					break;
 				case '*':
-					printStream.println(calcCore.multiply(a, b));
+					result = calcCore.multiply(a, b);
 					break;
 				case '/':
 					if (Double.compare(b, 0) == 0) {
-						printStream.println("Division by zero is not allowed.");
-						break;
+						printStream.println("Division by zero is not allowed.\n");
+						continue;
 					}
-					printStream.println(calcCore.divide(a, b));
+					result = calcCore.divide(a, b);
 					break;
+				default:
+					continue;
+			}
+
+			printStream.println(result);
+
+			stringBuilder.append(a).append(' ').append(operator).append(' ').append(b).append(" = ").append(result);
+			history.add(stringBuilder.toString());
+			stringBuilder.setLength(0);
+			if (history.size() > 5) {
+				history.poll();
 			}
 
 			printStream.println();
