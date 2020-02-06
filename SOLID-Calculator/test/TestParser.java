5e37eecb2bbdffef3ba2500a10106145435983d3
Ruslan Shakirov <shakirovrrr@outlook.com>
2020-02-06 15:03:13 +0300

    Finish TestParser

--- SOLID-Calculator/test/TestParser.java
+++ SOLID-Calculator/test/TestParser.java
@@ -27,5 +27,61 @@ public class TestParser {
 		assertValues(5.0, '+', 2.0);
 	}
 
+	@Test
+	void test2() {
+		boolean result;
+		parser.setInput("3-4");
+		result = parser.parse();
 
+		Assertions.assertTrue(result);
+		assertValues(3, '-', 4);
+	}
+
+	@Test
+	void test3() {
+		boolean result;
+		parser.setInput(" -5.227 *    36.6  ");
+		result = parser.parse();
+
+		Assertions.assertTrue(result);
+		assertValues(-5.227, '*', 36.6);
+	}
+
+	@Test
+	void test4() {
+		boolean result;
+		parser.setInput("10 / 0");
+		result = parser.parse();
+
+		Assertions.assertTrue(result);
+		assertValues(10, '/', 0);
+	}
+
+	@Test
+	void test5() {
+		boolean result;
+		parser.setInput("79yhuiweo");
+		result = parser.parse();
+
+		Assertions.assertFalse(result);
+	}
+
+	@Test
+	void test6() {
+		boolean result;
+		parser.setInput("64..5+ 100");
+		result = parser.parse();
+
+		Assertions.assertFalse(result);
+	}
+
+	@Test
+	void test7() {
+		boolean result;
+		parser.setInput("25.3 - 100.");
+		result = parser.parse();
+
+		Assertions.assertTrue(result);
+		assertValues(25.3, '-', 100.);
+	}
 }
